bea1ffbec573557160472dd7a05eb533b0aab5ff
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-29 22:40:18 -0500

    corrected the product.js able to add item into cart

--- src/App.js
+++ src/App.js
@@ -22,16 +22,10 @@ function App() {
     <ProductContext.Provider value={{ products, addItem }}>
       <CartContext.Provider value={cart}>
         <div className="App">
-          <Navigation cart={cart} />
-
+          <Navigation />
           {/* Routes */}
-          <Route
-            exact
-            path="/"
-            render={() => <Products products={products} addItem={addItem} />}
-          />
-
-          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
         </div>
       </CartContext.Provider>
     </ProductContext.Provider>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,12 @@
-import React, { useContext } from 'react';
-import ProductContext from '../context/ProductContext';
+import React from 'react';
 
-const Product = () => {
-  const { products, addItem } = useContext(ProductContext);
+const Product = props => {
   return (
-    <div className="products-container">
-      {products.map(product => (
-        <Product key={product.id} product={product} addItem={addItem} />
-      ))}
+    <div className="product">
+      <img src={props.product.image} alt={`${props.product.title} book`} />
+      <h1 className="title">{props.product.title}</h1>
+      <p className="price">${props.product.price}</p>
+      <button onClick={() => props.addItem(props.product)}>Add to cart</button>
     </div>
   );
 };
